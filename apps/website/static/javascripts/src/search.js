0a65cdbc420efa262720d96e772c66781f7ac168
ianzapolsky <ianzapolsky@gmail.com>
2014-07-31 01:58:36 -0400

    Add search javascript

--- apps/website/static/javascripts/src/search.js
+++ apps/website/static/javascripts/src/search.js
@@ -3,36 +3,87 @@
  * by Ian Zapolsky
  */
 
+
 (function() {
-  
-  var searchButton = $('#search');
-  
-  var resultsContainer = $('#results');
-   
-  // data to be scraped from form
-  var tempo,
-      tessitura,
-      songStyle,
-      gender,
-      characterType,
-      timePeriod;
 
-  va
+  var search = {
 
-  var search = function() {
-    
+    init: function() {
+      var _this = this;
+      
+      $(document).ready(function() {
+        _this.initializeElements();
+        _this.bindEvents();
+      });
+    },
+ 
+    initializeElements: function() {
+      this.$button = $('#search');
+      this.$tempo = $('#tempo');
+      this.$tessitura = $('#tessitura');
+      this.$songStyle = $('#songStyle');
+      this.$gender = $('#gender');
+      this.$characterType = $('#characterType');
+      this.$timePeriod = $('#timePeriod');
+    },
 
 
+    bindEvents: function() {
+      this.$button.on('click', $.proxy(this.sendRequest, this));
+    },
+        
+    buildURL: function() {
+      this.scrapeForm();
+      var url = '/songs/search?tempo=' + this.tempo + '&' +
+                'tessitura=' + this.tessitura + '&' +
+                'songStyle=' + this.songStyle + '&' +
+                'gender=' + this.gender + '&' +
+                'characterType=' + this.characterType + '&' +
+                'timePeriod=' + this.timePeriod;
+      return url;
+    },
+      
+    scrapeForm: function() {
+      this.tempo = this.$tempo.val() === '- - - -' ? '' : this.$tempo.val();
+      this.tessitura = this.$tessitura.val() === '- - - -' ? '' : this.$tessitura.val();
+      this.songStyle = this.$songStyle.val() === '- - - -' ? '' : this.$songStyle.val();
+      this.gender = this.$gender.val() === '- - - -' ? '' : this.$gender.val();
+      this.characterType = this.$characterType.val() === '- - - -' ? '' : this.$characterType.val();
+      this.timePeriod = this.$timePeriod.val() === '- - - -' ? '' : this.$timePeriod.val();
+    },
+
+    sendRequest: function() {
+      var url = this.buildURL();
+      var _this = this;
+      $.ajax({
+        url: url,
+        dataType: 'json', 
+        success: function(data) {
+          _this.renderData(data);
+        }
+      });
+    },
+
+    renderData: function(initData) {
+      
+      var results = document.querySelector('.results');
+      results.innerHTML = '';
+      for (var i = 0; i < initData.length; i++) {
+
+        var song = document.createElement('li');
+        var songDetail = document.createElement('p');
+        songDetail.innerHTML = initData[i].title;
+        song.appendChild(songDetail);
+        results.appendChild(song);
+      }
+
+    },
 
   }
 
-  var scrapeForm = function() {
-  
-    tempo = 
-    
-  
+  search.init();
 
-  }
+})();
 
-  
-  
+
+
